59ba6a57d43a470e4aea52ea0a9a1aac5bb97955
jeanEUB <jean.rodriguez@eub.edu.bh>
2026-07-07 10:23:42 +0300

    correcting Sign Out button functionality

--- app.js
+++ app.js
@@ -173,6 +173,7 @@ async function initializeAdmissionsPage() {
     loadRecords();
     initFilterDropdowns();
     renderFormFields();
+    bindAuthButtons();
     setupEventListeners();
     updateUI();
 }

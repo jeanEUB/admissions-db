bda3dad97478b5c6b241d5cac19e7efbd5d770f9
jeanEUB <jean.rodriguez@eub.edu.bh>
2026-07-07 09:51:42 +0300

    changed redirect URL to correct login page loop

--- auth-config.js
+++ auth-config.js
@@ -1,7 +1,7 @@
 window.AUTH_CONFIG = {
     clientId: 'e76eb0fc-3399-4b30-9bad-9ad62f739cf4',
     tenantId: '21768ddf-0611-496f-b848-cc9cfd167dce',
-    redirectUri: 'https://jeaneub.github.io/admissions-db/',
+    redirectUri: 'https://github.com/jeanEUB/admissions-db/blob/main/index.html',
     postLogoutRedirectUri: 'https://jeaneub.github.io/admissions-db/',
     cacheLocation: 'localStorage'
 };
